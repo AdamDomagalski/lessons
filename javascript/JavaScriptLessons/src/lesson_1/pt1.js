c0bba4c490e6e16620ef57d2c619fe59ec8bc019
Adam <domagalski.a@gmial.com>
2014-07-09 16:27:35 +0200

    initial commit

---
+++ javascript/JavaScriptLessons/src/lesson_1/pt1.js
@@ -0,0 +1,27 @@
+/**
+ *  8.07.2014 
+ 
+
+falsy values nulll undefined 0,-0 NaN w porównaniach są traktowanie jako false
+
+
+	null i undefined to nie jest to samo
+	null to pusty obiekt 
+	undefined to coś niezdefiniowanego, 
+	
+	null == undefined  - to jest true
+	sprawdzanie wartości
+	
+	null === undefined - to jest false
+	sprawdza typy I WARTOŚĆ!
+	
+	
+	do sprawdzania czy coś jest nullem lepiej robić
+	if(developr === null)
+	
+	
+	w funkcjach szukajac parametrów 
+	if(developr === undefined)
+	
+	`	
+*/
